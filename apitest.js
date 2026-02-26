af95b8261fe65d097de24b75cff2d037cecff53c
unknown <gajadeerav@gmail.com>
2026-02-26 16:20:40 +0530

    traffic code and neo4j test codes-2

--- apitest.js
+++ apitest.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-const API_KEY = "9ec70c722f973996c3bdf35ac454f56a";
+//const API_KEY = "9ec70c722f973996c3bdf35ac454f56a";
 
 async function getWeather(city) {
   const res = await axios.get(
